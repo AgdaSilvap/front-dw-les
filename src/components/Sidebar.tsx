4255af455189652e67cfc19511249f051f50a80f
Agda Silva <agdasilvap@gmail.com>
2025-06-03 20:25:46 -0300

    feat: add Sidebar component with profile image

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,10 @@
+
+export const Sidebar = () => {
+  return (
+    <>
+      <aside className="container d-inline-block h-100 mx-0 mt-0" id="side-bar">
+        <img src="/image.png" alt="foto-perfil" className="border border-2 border-light rounded-circle" id="imagem-perfil" />
+      </aside>
+    </>
+  )
+}
