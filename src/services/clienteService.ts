b38ffe6db23c08d728f9563537a2638f260e0e50
Bianca Rodrigues <iambiancarodrigues@gmail.com>
2025-06-13 07:56:57 -0300

    [feat] service do cliente

---
+++ src/services/clienteService.ts
@@ -0,0 +1,33 @@
+import { api } from "./api";
+
+export interface Cliente {
+  nome: string;
+  nascimento: string;
+  genero: string;
+  cpf: string;
+  telefone: string;
+  endereco: string;
+}
+
+export const ClienteService = {
+  create: async (cliente: Cliente) => {
+    const response = await api.post("/clientes", cliente);
+    return response.data;
+  },
+  getAll: async () => {
+    const response = await api.get("/clientes");
+    return response.data;
+  },
+  getOne: async (id: string) => {
+    const response = await api.get(`/clientes/${id}`);
+    return response.data;
+  },
+  update: async (id: string, cliente: Cliente) => {
+    const response = await api.put(`/clientes/${id}`, cliente);
+    return response.data;
+  },
+  delete: async (id: string) => {
+    const response = await api.delete(`/clientes/${id}`);
+    return response.data;
+  }
+}
